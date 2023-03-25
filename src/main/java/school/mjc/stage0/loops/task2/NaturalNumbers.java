b030ba192cb58be49ad3eadad6e3ccc487f17617
rodionM87 <65753876+rodionM87@users.noreply.github.com>
2023-03-25 21:06:11 +0600

    Update NaturalNumbers.java

--- src/main/java/school/mjc/stage0/loops/task2/NaturalNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/NaturalNumbers.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.loops.task2;
 
 public class NaturalNumbers {
     public void naturalNumbersPrinter(int lastPrinted) {
+        start = 0;
+        while(start <= lastPrinted) {
+            System.out.println(start);
     }
 }
