1b5a84bb5e780cc3f684b1e5887c24e95ec88eb6
rodionM87 <65753876+rodionM87@users.noreply.github.com>
2023-03-27 15:57:39 +0600

    Update PrimeNumbers.java

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -5,7 +5,7 @@ public class PrimeNumbers {
         int start = 0;
         while(start <= printToInclusive) {
             if (start == 0) {
-                start++;
+                start = start + 2;
                 continue;
             }
             else if(start%start==0 && (start%2!=0 && start%3!=0 && start%5!=0) || (start==2) || (start==3) || (start==5)) {
